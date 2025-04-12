c07e1e766b199c518e6a393a81b2ee9ef3042d42
guhuo <1347501570@qq.com>
2025-04-12 20:20:20 +0800

    模块初始化

---
+++ gulimall-member/src/test/java/com/guhuo/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guhuo.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
